283e2eace7bde9e99263e43f57c3a20547035218
Pontus Bengtsson <100732193+Nobody0163@users.noreply.github.com>
2025-09-19 06:16:48 +0000

    Add Transform3D

---
+++ main/math/transform3d.cpp
@@ -0,0 +1,44 @@
+#include "transform3d.hpp"
+
+#include <stdexcept>
+
+namespace Chloroform::Math {
+	Transform3D::Transform3D(Vector3 offset, Vector3 basis_x, Vector3 basis_y, Vector3 basis_z) : offset(offset), basis_x(basis_x), basis_y(basis_y), basis_z(basis_z) {}
+
+	Vector3 Transform3D::operator*(const Vector3& other) const {
+		return Vector3(
+			this->offset.x + this->basis_x.x * other.x + this->basis_y.x * other.y + this->basis_z.x * other.z,
+			this->offset.y + this->basis_x.y * other.x + this->basis_y.y * other.y + this->basis_z.y * other.z,
+			this->offset.z + this->basis_x.z * other.x + this->basis_y.z * other.y + this->basis_z.z * other.z
+		);
+	}
+
+	Transform3D Transform3D::operator*(const Transform3D& other) const {
+		Vector3 new_offset = (*this) * other.offset;
+
+		Vector3 new_basis_x = Vector3(
+			this->basis_x.x * other.basis_x.x + this->basis_y.x * other.basis_x.y + this->basis_z.x * other.basis_x.z,
+			this->basis_x.y * other.basis_x.x + this->basis_y.y * other.basis_x.y + this->basis_z.y * other.basis_x.z,
+			this->basis_x.z * other.basis_x.x + this->basis_y.z * other.basis_x.y + this->basis_z.z * other.basis_x.z
+		);
+
+		Vector3 new_basis_y = Vector3(
+			this->basis_x.x * other.basis_y.x + this->basis_y.x * other.basis_y.y + this->basis_z.x * other.basis_y.z,
+			this->basis_x.y * other.basis_y.x + this->basis_y.y * other.basis_y.y + this->basis_z.y * other.basis_y.z,
+			this->basis_x.z * other.basis_y.x + this->basis_y.z * other.basis_y.y + this->basis_z.z * other.basis_y.z
+		);
+
+		Vector3 new_basis_z = Vector3(
+			this->basis_x.x * other.basis_z.x + this->basis_y.x * other.basis_z.y + this->basis_z.x * other.basis_z.z,
+			this->basis_x.y * other.basis_z.x + this->basis_y.y * other.basis_z.y + this->basis_z.y * other.basis_z.z,
+			this->basis_x.z * other.basis_z.x + this->basis_y.z * other.basis_z.y + this->basis_z.z * other.basis_z.z
+		);
+
+		return Transform3D(new_offset, new_basis_x, new_basis_y, new_basis_z);
+	}
+
+	Transform3D& Transform3D::operator*=(const Transform3D& other) {
+		*this = (*this) * other;
+		return *this;
+	}
+}
---
+++ main/math/transform3d.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "vector3.hpp"
+
+namespace Chloroform::Math {
+	class Transform3D {
+	public:
+		Vector3 offset;
+		Vector3 basis_x;
+		Vector3 basis_y;
+		Vector3 basis_z;
+
+		Transform3D(Vector3 offset = Vector3(0, 0, 0), Vector3 basis_x = Vector3(1, 0, 0), Vector3 basis_y = Vector3(0, 1, 0), Vector3 basis_z = Vector3(0, 0, 1));
+
+		Vector3 operator*(const Vector3& other) const;
+		Transform3D operator*(const Transform3D& other) const;
+		Transform3D& operator*=(const Transform3D& other);
+	};
+}
